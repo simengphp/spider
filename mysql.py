41cf4f1fde471a70279eb2a9cf10a70a9726b33a
刘柱 <476319748@qq.com>
2017-11-16 18:07:10 +0800

    爬虫

--- mysql.py
+++ mysql.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
-# encoding: utf8
+# encoding: utf-8
+import sys
 import MySQLdb
-db = MySQLdb.connect("localhost", "root", "", "sql")
+
+
+db = MySQLdb.connect("localhost", "root", "", "sql", charset="utf8")
 cursor = db.cursor()
 cursor.execute("select * from tb_article")
 data = cursor.fetchall()
-print data
+# print data
+for row in data:
+    fname = row[0]
+    lname = row[1]
+    # 打印结果
+    print "fname=%s,lname=%s" % \
+          (fname, lname)
 db.close()
